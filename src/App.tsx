9a765fb7a3f5dd85266ba331b3a527157d8a368a
robertbajo-png <robertbajo@gmail.com>
2025-11-01 22:49:14 +0100

    Add dedicated about page and route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import About from "./pages/About";
 import WineSnap from "./pages/WineSnap";
 import NotFound from "./pages/NotFound";
 import History from "./pages/History";
@@ -18,7 +18,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WineSnap />} />
-          <Route path="/om" element={<Index />} />
+          <Route path="/om" element={<About />} />
           <Route path="/historik" element={<History />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
